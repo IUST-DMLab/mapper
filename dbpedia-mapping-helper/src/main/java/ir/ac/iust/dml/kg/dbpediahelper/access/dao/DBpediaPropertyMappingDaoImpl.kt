d7470a6b3af2e5f92c64f54934be16891037e0d8
Majid Asgari <majid.asgari@gmail.com>
2017-02-22 13:25:27 +0330

    add hasClass parameter to dao

--- dbpedia-mapping-helper/src/main/java/ir/ac/iust/dml/kg/dbpediahelper/access/dao/DBpediaPropertyMappingDaoImpl.kt
+++ dbpedia-mapping-helper/src/main/java/ir/ac/iust/dml/kg/dbpediahelper/access/dao/DBpediaPropertyMappingDaoImpl.kt
@@ -12,55 +12,57 @@ import org.springframework.stereotype.Repository
 @Repository
 open class DBpediaPropertyMappingDaoImpl : DBpediaPropertyMappingDao {
 
-    @Suppress("UNCHECKED_CAST")
-    override fun read(language: String?, clazz: String?, type: String?,
-                      like: Boolean, templateProperty: String?, ontologyProperty: String?):
-            MutableList<DBpediaPropertyMapping> {
-        val session = this.sessionFactory.openSession()
-        val criteria = session.createCriteria(DBpediaPropertyMapping::class.java)
-        if (language != null) criteria.add(Restrictions.eq("language", language))
-        if (clazz != null) criteria.add(Restrictions.eq("clazz", clazz))
-        if (type != null) criteria.add(Restrictions.eq("type", type))
-        if (like) {
-            if (templateProperty != null)
-                criteria.add(Restrictions.like("templateProperty", "%$templateProperty%"))
-            if (ontologyProperty != null)
-                criteria.add(Restrictions.like("ontologyProperty", "%$ontologyProperty%"))
-        } else {
-            if (templateProperty != null)
-                criteria.add(Restrictions.eq("templateProperty", templateProperty))
-            if (ontologyProperty != null)
-                criteria.add(Restrictions.eq("ontologyProperty", ontologyProperty))
-        }
-        val mapping = criteria.list() as MutableList<DBpediaPropertyMapping>
-        session.close()
-        return mapping
-    }
+   @Suppress("UNCHECKED_CAST")
+   override fun read(language: String?, clazz: String?, type: String?,
+                     like: Boolean, hasClass: Boolean, templateProperty: String?, ontologyProperty: String?):
+         MutableList<DBpediaPropertyMapping> {
+      val session = this.sessionFactory.openSession()
+      val criteria = session.createCriteria(DBpediaPropertyMapping::class.java)
+      if (language != null) criteria.add(Restrictions.eq("language", language))
+      if (clazz != null) criteria.add(Restrictions.eq("clazz", clazz))
+      if (type != null) criteria.add(Restrictions.eq("type", type))
+      if (hasClass) criteria.add(Restrictions.isNotNull("clazz"))
+      if (like) {
+         if (templateProperty != null)
+            criteria.add(Restrictions.like("templateProperty", "%$templateProperty%"))
+         if (ontologyProperty != null)
+            criteria.add(Restrictions.like("ontologyProperty", "%$ontologyProperty%"))
+      } else {
+         if (templateProperty != null)
+            criteria.add(Restrictions.eq("templateProperty", templateProperty))
+         if (ontologyProperty != null)
+            criteria.add(Restrictions.eq("ontologyProperty", ontologyProperty))
+      }
+      val mapping = criteria.list() as MutableList<DBpediaPropertyMapping>
+      session.close()
+      return mapping
+   }
 
-    @Autowired
-    lateinit var sessionFactory: SessionFactory
+   @Autowired
+   lateinit var sessionFactory: SessionFactory
 
-    override fun save(p: DBpediaPropertyMapping) {
-        val session = this.sessionFactory.openSession()
-        val tx = session.beginTransaction()
-        session.saveOrUpdate(p)
-        tx.commit()
-        session.close()
-    }
+   override fun save(p: DBpediaPropertyMapping) {
+      val session = this.sessionFactory.openSession()
+      val tx = session.beginTransaction()
+      session.saveOrUpdate(p)
+      tx.commit()
+      session.close()
+   }
 
-    @Suppress("UNCHECKED_CAST")
-    override fun list(pageSize: Int, page: Int): PagedData<DBpediaPropertyMapping> {
-        val session = this.sessionFactory.openSession()
-        val list = SqlJpaTools.page(DBpediaPropertyMapping::class.java, page, pageSize, session)
-        session.close()
-        return list
-    }
+   @Suppress("UNCHECKED_CAST")
+   override fun list(pageSize: Int, page: Int, hasClass: Boolean): PagedData<DBpediaPropertyMapping> {
+      val session = this.sessionFactory.openSession()
+      val criteria = SqlJpaTools.condtionalCriteria(hasClass, Restrictions.isNotNull("clazz"))
+      val list = SqlJpaTools.page(DBpediaPropertyMapping::class.java, page, pageSize, session, *criteria)
+      session.close()
+      return list
+   }
 
-    @Suppress("UNCHECKED_CAST")
-    override fun listTemplatePropertyMapping(pageSize: Int, page: Int): PagedData<TemplatePropertyMapping> {
-        val session = this.sessionFactory.openSession()
-        val list = SqlJpaTools.page(TemplatePropertyMapping::class.java, page, pageSize, session)
-        session.close()
-        return list
-    }
+   @Suppress("UNCHECKED_CAST")
+   override fun listTemplatePropertyMapping(pageSize: Int, page: Int): PagedData<TemplatePropertyMapping> {
+      val session = this.sessionFactory.openSession()
+      val list = SqlJpaTools.page(TemplatePropertyMapping::class.java, page, pageSize, session)
+      session.close()
+      return list
+   }
 }
