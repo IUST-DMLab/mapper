3d0e2273f49489ed68efdb75e6b5160fbcc6aae5
Majid Asgari <majid.asgari@gmail.com>
2017-04-14 12:42:36 +0430

    add compiler plugin to maven add file logger to logback add commander to run commands

--- runner/src/main/java/ir/ac/iust/dml/kg/mapper/runner/Application.java
+++ runner/src/main/java/ir/ac/iust/dml/kg/mapper/runner/Application.java
@@ -1,5 +1,6 @@
 package ir.ac.iust.dml.kg.mapper.runner;
 
+import ir.ac.iust.dml.kg.mapper.runner.commander.Commander;
 import ir.ac.iust.dml.kg.mapper.runner.web.Jackson2ObjectMapperPrettier;
 import ir.ac.iust.dml.kg.mapper.runner.web.filter.FilterRegistrationConfiguration;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+
+import java.util.Properties;
 
 @SpringBootApplication
 @ImportResource(value = {
@@ -26,7 +30,17 @@ import org.springframework.context.annotation.ImportResource;
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication app = new SpringApplication(Application.class);
+    Properties properties = new Properties();
+    if (args.length > 0) properties.put("server.port", 9999);
+    else properties.put("server.port", 8090);
+    app.setDefaultProperties(properties);
+    ConfigurableApplicationContext context = app.run(args);
+    if (args.length > 0) {
+      Commander commander = context.getBeansOfType(Commander.class).values().iterator().next();
+      commander.processArgs(args[0], args.length > 1 ? args[1] : null);
+    }
+
   }
 
 }
---
+++ runner/src/main/java/ir/ac/iust/dml/kg/mapper/runner/commander/Commander.kt
@@ -0,0 +1,39 @@
+package ir.ac.iust.dml.kg.mapper.runner.commander
+
+import ir.ac.iust.dml.kg.dbpediahelper.logic.TripleImporter
+import ir.ac.iust.dml.kg.mapper.runner.web.rest.MappingHelperRestServices
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class Commander {
+   @Autowired
+   lateinit var services: MappingHelperRestServices
+   private val logger = Logger.getLogger(this.javaClass)!!
+
+   fun processArgs(command: String, arg: String? = null) {
+      logger.info("====================================")
+      logger.info("====================================")
+      logger.info("running command $command at ${Date()}")
+      logger.info("====================================")
+      logger.info("====================================")
+      try {
+         when (command) {
+            "load" -> services.load()
+            "loadTypes" -> services.loadTypes()
+            "createStatsFile" -> services.createStatsFile()
+            "writeStats" -> services.writeStats()
+            "generateMapping" -> services.generateMapping()
+            "triples" -> services.triples(TripleImporter.StoreType.valueOf(arg!!))
+            "redirects" -> services.redirects()
+            "entities" -> services.entities()
+         }
+      } catch (th: Throwable) {
+         logger.error(th)
+      } finally {
+         System.exit(0)
+      }
+   }
+}
