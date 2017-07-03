aebee772770b7717f10f6708605b68a417fa222e
Majid Asgari <majid.asgari@gmail.com>
2017-07-04 02:11:49 +0430

    resolving duplicated names and write disambiguities

--- mapper/src/main/java/ir/ac/iust/dml/kg/dbpediahelper/logic/store/ManualInsertLogic.kt
+++ mapper/src/main/java/ir/ac/iust/dml/kg/dbpediahelper/logic/store/ManualInsertLogic.kt
@@ -21,6 +21,7 @@ class ManualInsertLogic {
   private val rdfType = PrefixService.prefixToUri(PrefixService.TYPE_URL)!!
   private val rdfsLabel = PrefixService.prefixToUri(PrefixService.LABEL_URL)!!
   private val fkgVariantLabel = PrefixService.prefixToUri(PrefixService.VARIANT_LABEL_URL)!!
+  private val disambiguatedFrom = PrefixService.prefixToUri(PrefixService.DISAMBIGUATED_FROM_URI)!!
   private val owlObjectProperty = PrefixService.prefixToUri(PrefixService.TYPE_OF_ALL_PROPERTIES)!!
   private val owlNamedIndividual = PrefixService.prefixToUri(PrefixService.TYPE_OF_ALL_RESOURCES)!!
 
@@ -34,8 +35,15 @@ class ManualInsertLogic {
   fun saveResource(resourceUrl: String, ontologyClass: String?, label: String?, variantLabel: String?, permanent: Boolean): Boolean {
     val url = if (!resourceUrl.startsWith("http://")) PrefixService.getFkgResourceUrl(resourceUrl) else resourceUrl
     saveTriple(url, rdfType, owlNamedIndividual, permanent)
-    if (label != null) saveTriple(url, rdfsLabel, label, permanent)
-    if (variantLabel != null) saveTriple(url, fkgVariantLabel, variantLabel, permanent)
+    if (label != null) {
+      saveTriple(url, rdfsLabel, label, permanent)
+      saveTriple(url, fkgVariantLabel, label, permanent)
+      checkAmbiguity(url, label)
+    }
+    if (variantLabel != null) {
+      saveTriple(url, fkgVariantLabel, variantLabel, permanent)
+      checkAmbiguity(url, variantLabel)
+    }
     if (ontologyClass != null) {
       val tree = classLogic.getTree(ontologyClass)
       tree?.split("/")?.forEach { saveTriple(url, rdfType, PrefixService.convertFkgOntologyUrl(it), permanent) }
@@ -46,9 +54,35 @@ class ManualInsertLogic {
   fun savePredicate(predicateUrl: String, label: String?, variantLabel: String?, permanent: Boolean): Boolean {
     val url = if (!predicateUrl.startsWith("http://")) PrefixService.getFkgOntologyPropertyUrl(predicateUrl) else predicateUrl
     saveTriple(url, rdfType, owlObjectProperty, permanent)
-    if (label != null) saveTriple(url, rdfsLabel, label, permanent)
-    if (variantLabel != null) saveTriple(url, fkgVariantLabel, variantLabel, permanent)
+    if (label != null) {
+      saveTriple(url, rdfsLabel, label, permanent)
+      saveTriple(url, fkgVariantLabel, label, permanent)
+      checkAmbiguity(url, label)
+    }
+    if (variantLabel != null) {
+      saveTriple(url, fkgVariantLabel, variantLabel, permanent)
+      checkAmbiguity(url, variantLabel)
+    }
     return true
+  }
+
+  // TODO share this piece of code with PredicateImporter
+  fun checkAmbiguity(subject: String, label: String) {
+    val result = kgDao.read(predicate = fkgVariantLabel, objekt = label)
+        .filter { triple -> triple.objekt == label && triple.subject != subject }
+    if (result.isNotEmpty()) {
+      saveAll(source = subject, subject = subject, property = disambiguatedFrom, objeck = label)
+      result.forEach {
+        saveAll(source = it.source ?: it.subject!!, subject = it.subject!!,
+            property = disambiguatedFrom, objeck = it.objekt!!)
+      }
+    }
+  }
+
+  fun saveAll(source: String, subject: String, objeck: String, property: String) {
+    kgDao.save(source, subject, objeck, property)
+    (kgDao as KnowledgeStoreFkgTripleDaoImpl).flush()
+    virtuosoDao.save(source, subject, objeck, property)
   }
 
   fun saveTriple(subjectUrl: String, predicateUrl: String, objectUrl: String, permanent: Boolean): Boolean {
