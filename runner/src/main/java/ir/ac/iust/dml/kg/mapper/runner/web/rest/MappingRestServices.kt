b7662f00f6aefc45faf99253621bedcaf1391f9f
Majid Asgari <majid.asgari@gmail.com>
2017-07-24 19:52:53 +0430

    using gson expect jackson to generate search result

--- runner/src/main/java/ir/ac/iust/dml/kg/mapper/runner/web/rest/MappingRestServices.kt
+++ runner/src/main/java/ir/ac/iust/dml/kg/mapper/runner/web/rest/MappingRestServices.kt
@@ -1,22 +1,26 @@
 package ir.ac.iust.dml.kg.mapper.runner.web.rest
 
+import com.google.gson.GsonBuilder
 import io.swagger.annotations.Api
 import ir.ac.iust.dml.kg.mapper.logic.store.KSMappingLogic
 import ir.ac.iust.dml.kg.services.client.swagger.model.TemplateData
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
+
 @RestController
 @RequestMapping("/mapping/rest/v2/")
 @Api(tags = arrayOf("mapping"), description = "سرویس‌های ویرایش نگاشت")
 class MappingRestServices {
   @Autowired lateinit var logic: KSMappingLogic
+  val gson = GsonBuilder().setPrettyPrinting().create()
 
   @RequestMapping("insert", method = arrayOf(RequestMethod.POST))
   @ResponseBody
   fun insert(@RequestBody data: TemplateData) = logic.insert(data)
 
-  @RequestMapping("search", method = arrayOf(RequestMethod.GET))
+  @RequestMapping("search", method = arrayOf(RequestMethod.GET),
+      produces = arrayOf("application/json; charset=utf-8"))
   @ResponseBody
   fun search(@RequestParam(defaultValue = "0") page: Int,
              @RequestParam(defaultValue = "20") pageSize: Int,
@@ -28,9 +32,11 @@ class MappingRestServices {
              @RequestParam(required = false, defaultValue = "false") propertyNameLike: Boolean,
              @RequestParam(required = false) predicateName: String?,
              @RequestParam(required = false, defaultValue = "false") predicateNameLike: Boolean,
-             @RequestParam(required = false) approved: Boolean?) =
-      logic.search(page, pageSize, templateName, templateNameLike, className, classNameLike,
-          propertyName, propertyNameLike, predicateName, predicateNameLike, approved)
+             @RequestParam(required = false) approved: Boolean?): String {
+    val result = logic.search(page, pageSize, templateName, templateNameLike, className, classNameLike,
+        propertyName, propertyNameLike, predicateName, predicateNameLike, approved)
+    return gson.toJson(result)
+  }
 
   @RequestMapping("searchProperty", method = arrayOf(RequestMethod.GET))
   @ResponseBody
