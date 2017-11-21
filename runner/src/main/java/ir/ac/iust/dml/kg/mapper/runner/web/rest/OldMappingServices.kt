9dd26267c7b46b2742cf328a152dc019974d2810
Majid Asgari <majid.asgari@gmail.com>
2017-11-21 16:44:52 +0330

    move store connection configuration to config file

--- runner/src/main/java/ir/ac/iust/dml/kg/mapper/runner/web/rest/OldMappingServices.kt
+++ runner/src/main/java/ir/ac/iust/dml/kg/mapper/runner/web/rest/OldMappingServices.kt
@@ -7,6 +7,7 @@
 package ir.ac.iust.dml.kg.mapper.runner.web.rest
 
 import io.swagger.annotations.Api
+import ir.ac.iust.dml.kg.raw.utils.ConfigReader
 import ir.ac.iust.dml.kg.raw.utils.URIs
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -17,6 +18,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/mapping/rest/v1/")
 @Api(tags = arrayOf("propertyMapping"), description = "سرویس‌های نگاشت خصیصه")
 class OldMappingServices {
+
+  private val prefixAddresses = mutableMapOf<String, String>()
+
+  init {
+    val fkgPrefixConvert = ConfigReader.getString("fkg.prefix.convert", "fkg")
+    URIs.prefixAddresses.forEach {
+      prefixAddresses[it.key.replace("fkg", fkgPrefixConvert)] = it.value
+    }
+  }
 
   @RequestMapping("prefixes", method = arrayOf(RequestMethod.GET))
   @ResponseBody
